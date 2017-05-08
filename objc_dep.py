a12d118634160891d3039f0d789a655fe76a6ab7
yuryybk <yury@Yurys-MacBook-Pro.local>
2017-05-08 10:55:23 +0300

    Add option to group classes and class categories to single entity; add option to skip list of specified nodes (by names)

--- objc_dep.py
+++ objc_dep.py
@@ -40,7 +40,7 @@ def gen_filenames_imported_in_file(path, regex_exclude, system, extensions):
             yield (filename + extension) if extension else filename
 
 
-def dependencies_in_project(path, ext, exclude, ignore, system, extensions):
+def dependencies_in_project(path, ext, exclude, ignore, system, extensions, group_categories, nodes_to_skip):
     d = {}
     
     regex_exclude = None
@@ -62,6 +62,14 @@ def dependencies_in_project(path, ext, exclude, ignore, system, extensions):
             if regex_exclude is not None and regex_exclude.search(filename):
                 continue
 
+            if group_categories:
+                if "+" in filename:
+                    index = filename.find("+", 0, len(filename))
+                    filename = filename[0:index]
+
+            if filename in nodes_to_skip:
+                continue
+
             if filename not in d:
                 d[filename] = Set()
             
@@ -70,23 +78,38 @@ def dependencies_in_project(path, ext, exclude, ignore, system, extensions):
             for imported_filename in gen_filenames_imported_in_file(path, regex_exclude, system, extensions):
                 if imported_filename != filename:
                     imported_filename = imported_filename if extensions else os.path.splitext(imported_filename)[0]
-                    d[filename].add(imported_filename)
 
-                if "/" not in imported_filename:
-                    d[filename].add(imported_filename)
-                else:
+                if group_categories:
+                    if "+" in imported_filename:
+                        index = imported_filename.find("+", 0, len(imported_filename))
+                        imported_filename = imported_filename[0:index]
+
+                if "/" in imported_filename:
                     index = imported_filename.rfind("/", 0, len(imported_filename))
-                    imported_filename_substring = imported_filename[index + 1:]
-                    d[filename].add(imported_filename_substring)
+                    imported_filename = imported_filename[index + 1:]
+
+                if imported_filename in nodes_to_skip:
+                    continue
+
+                d[filename].add(imported_filename)
+
     return d
 
 
-def dependencies_in_project_with_file_extensions(path, exts, exclude, ignore, system, extensions, root_class):
+def dependencies_in_project_with_file_extensions(path,
+                                                 exts,
+                                                 exclude,
+                                                 ignore,
+                                                 system,
+                                                 extensions,
+                                                 root_class,
+                                                 group_categories,
+                                                 nodes_to_skip):
 
     d = {}
     
     for ext in exts:
-        d2 = dependencies_in_project(path, ext, exclude, ignore, system, extensions)
+        d2 = dependencies_in_project(path, ext, exclude, ignore, system, extensions, group_categories, nodes_to_skip)
         for (k, v) in d2.iteritems():
             if not k in d:
                 d[k] = Set()
@@ -183,16 +206,24 @@ def print_frequencies_chart(d):
         sys.stderr.write(s)
 
 
-def dependencies_in_dot_format(path, exclude, ignore, system, extensions, root_class):
+def dependencies_in_dot_format(path, exclude, ignore, system, extensions, root_class, group_categories, nodes_to_skip):
     
-    d = dependencies_in_project_with_file_extensions(path, objc_extensions, exclude, ignore, system, extensions, root_class)
+    d = dependencies_in_project_with_file_extensions(path,
+                                                     objc_extensions,
+                                                     exclude,
+                                                     ignore,
+                                                     system,
+                                                     extensions,
+                                                     root_class,
+                                                     group_categories,
+                                                     nodes_to_skip)
 
     two_ways_set = two_ways_dependencies(d)
     untraversed_set = untraversed_files(d)
 
     category_list, d = category_files(d)
 
-    pch_set = dependencies_in_project(path, '.pch', exclude, ignore, system, extensions)
+    pch_set = dependencies_in_project(path, '.pch', exclude, ignore, system, extensions, group_categories, nodes_to_skip)
 
     #
     
@@ -249,22 +280,53 @@ def dependencies_in_dot_format(path, exclude, ignore, system, extensions, root_c
     l.append("}\n")
     return '\n'.join(l)
 
+
+def read_nodes_to_skip(path):
+    nodes = Set()
+    for line in open(path):
+        nodes.add(line.replace('\n',''))
+
+    return nodes
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-x", "--exclude", nargs='?', default='' ,help="regular expression of substrings to exclude from module names")
     parser.add_argument("-i", "--ignore", nargs='*', help="list of subfolder names to ignore")
     parser.add_argument("-s", "--system", action='store_true', default=False, help="include system dependencies")
     parser.add_argument("-e", "--extensions", action='store_true', default=False, help="print file extensions")
+    parser.add_argument("-g", "--group", action='store_true', default=False, help="group class and class categories together to manage like single entity")
     parser.add_argument("-r", "--root", default='', help="Class to use as root of dependency graph")
+    parser.add_argument("-k", "--skip", default='', help="skip nodes with names from pointed file")
     parser.add_argument("-t", "--test", action='store_true', default=False, help="Exit with a success status of 0 if no two-way dependencies exist or failure otherwise, instead of outputting a graph")
     parser.add_argument("project_path", help="path to folder hierarchy containing Objective-C files")
     args= parser.parse_args()
 
+    nodes_skip = []
+    if args.skip:
+        nodes_skip = read_nodes_to_skip(args.skip)
+
     if not args.test:
-        print dependencies_in_dot_format(args.project_path, args.exclude, args.ignore, args.system, args.extensions, args.root)
+        print dependencies_in_dot_format(args.project_path,
+                                         args.exclude,
+                                         args.ignore,
+                                         args.system,
+                                         args.extensions,
+                                         args.root,
+                                         args.group,
+                                         nodes_skip)
+
     else:
         # Test if two-way dependencies exist. If none do, then exit on success (0), otherwise exit on failure (1)
-        d = dependencies_in_project_with_file_extensions(args.project_path, objc_extensions, args.exclude, args.ignore, args.system, args.extensions, args.root)
+        d = dependencies_in_project_with_file_extensions(args.project_path,
+                                                         objc_extensions,
+                                                         args.exclude,
+                                                         args.ignore,
+                                                         args.system,
+                                                         args.extensions,
+                                                         args.root,
+                                                         args.group,
+                                                         nodes_skip)
         two_ways_set = two_ways_dependencies(d)
 
         if len(two_ways_set) == 0:
